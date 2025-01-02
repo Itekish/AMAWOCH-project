ca73a0d490d82e07687a313b2367c29cc787ebfe
Itekish <iteoluwakiisifadenipo@gmail.com>
2025-01-02 07:33:08 +0100

    1st commit

---
+++ landingPage.js
@@ -0,0 +1,19 @@
+homePage = document.getElementById("home")
+aboutPage = document.getElementById("about")
+signupPage = document.getElementById("signUp")
+getStarted = document.getElementById("getStarted")
+
+homePage.addEventListener('click', ()=> {
+    window.location.href = "./otherHtmlFiles/home.html"
+})
+aboutPage.addEventListener('click', ()=> {
+    window.location.href ="./otherHtmlFiles/about.html"
+})
+signupPage.addEventListener('click', ()=> {
+    window.location.href = "./otherHtmlFiles/signUp&logIn.html"
+})
+getStarted.addEventListener('click', ()=> {
+    window.location.href = "./otherHtmlFiles/signUp&logIn.html"
+})
+homePage.addEventListener('click', ()=> {})
+homePage.addEventListener('click', ()=> {})
